57712c6029ca96453fe11dc35f0f8654c45d378a
Daki <kalidoudaki@gmail.com>
2022-06-21 11:21:15 +0000

    change

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
-class Square
-    """The Square class will help us to  many things depend of it (area computation, etc.), so you, as class builder, must control the type and value of this attribute. One way to have the control is to keep it privately. You will see in next tasks how to get, update and validate the size value""" 
+class Square :
+    """
+    The Square class will help us to  many things depend of it (area computation, etc.), so you, as class builder, must control the type and value of this attribute.
+    One way to have the control is to keep it privately. You will see in next tasks how to get, update and validate the size value
+    """ 
